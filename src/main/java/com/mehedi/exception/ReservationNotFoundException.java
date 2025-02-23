ab8441ac39a1d4cc4810fd20b56aaa73c157558a
Mehedi Hasan (Parvez) <hasanmehedi185@gmail.com>
2025-02-23 13:46:10 +0600

    Backend: Search books by title, added more reservation functionalities
    * Book reservation
    * find reserved books by user
    * Search book by title
    * delete user

--- src/main/java/com/mehedi/exception/ReservationNotFoundException.java
+++ src/main/java/com/mehedi/exception/ReservationNotFoundException.java
@@ -2,6 +2,10 @@ package com.mehedi.exception;
 
 public class ReservationNotFoundException extends RuntimeException {
 
+    public ReservationNotFoundException() {
+        super("Reservation not found.");
+    }
+
     public ReservationNotFoundException(String message) {
         super(message);
     }
